a47814d771c9c3dffa7bec27489301e4dce22f54
Evgeny Artemov <eartemov013@gmail.com>
2023-01-28 04:02:05 +0400

    add Item definition

---
+++ Entities/Item.cs
@@ -0,0 +1,14 @@
+namespace Catalog.Entities;
+
+// record has better support for immutable objects
+//  - With expression support
+//  - value based equality support
+public record Item
+{
+    // init is for property values that we allow to set only on initialization 
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public decimal Price { get; init; }
+    public DateTimeOffset CreatedDate  { get; init; }
+}
+
