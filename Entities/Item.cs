f7347215c07a9e5906c38edd82953147e184688e
Evgeny Artemov <eartemov013@gmail.com>
2023-02-15 22:32:17 +0400

    add Items Controller

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -7,7 +7,7 @@ public record Item
 {
     // init is for property values that we allow to set only on initialization 
     public Guid Id { get; init; }
-    public string Name { get; init; }
+    public string Name { get; init; } = default!;
     public decimal Price { get; init; }
     public DateTimeOffset CreatedDate  { get; init; }
 }
