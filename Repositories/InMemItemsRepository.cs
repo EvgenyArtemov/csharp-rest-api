885bc703e3b4320eac7e1ec2f4225d91724c6394
Evgeny Artemov <eartemov013@gmail.com>
2023-02-15 22:32:51 +0400

    change project settings

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -4,11 +4,11 @@ namespace Catalog.Repositories;
 
 class InMemItemsRepository
 {
-    private readonly List<Item> _items = new List<Item>()
+    private readonly List<Item> _items = new()
     {
         new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
         new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
-        new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
+        new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
     };
 
     public IEnumerable<Item> GetItems()
@@ -21,4 +21,6 @@ class InMemItemsRepository
         return _items.SingleOrDefault(item => item.Id == id);
     }
 }
+
+
 
